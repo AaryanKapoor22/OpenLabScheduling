ec0023b709767a6ffe17a0f36668d9b554270b59
kevin-rodri <kr399880@gmail.com>
2023-11-20 01:19:31 -0500

    added upload form and booking component to site

---
+++ book.js
@@ -0,0 +1,24 @@
+const bookedButton = document.getElementById('missed');
+const openButton = document.getElementById('open');
+
+const missedContent = document.getElementById('missed-content');
+const openContent = document.getElementById('open-content');
+
+bookedButton.addEventListener("click", bookedClick);
+openButton.addEventListener("click", openClick);
+
+
+function bookedClick() {
+    bookedButton.classList.add('active-link', 'active');
+    openButton.classList.remove('active-link', 'active');
+    missedContent.hidden = false;
+    openContent.hidden = true;
+}
+
+function openClick() {
+    openButton.classList.add('active-link', 'active');
+    bookedButton.classList.remove('active-link', 'active');
+    openContent.hidden = false;
+    missedContent.hidden = true;
+}
+
