6ac33799fa973b496a8c64449c664e12617306b7
NovaMatthew <mnova412@gamil.com>
2023-11-30 13:53:03 -0500

    lab Manager and Open lab booking done

--- book.js
+++ book.js
@@ -22,6 +22,56 @@ function openClick() {
     missedContent.hidden = true;
 }
 
-function submitForm() {
-    window.location.href = 'upload.html';
-  }
+
+$('#bookingModal1').on('show.bs.modal', function (e) {
+    var button = $(e.relatedTarget); // Button that triggered the modal
+    var sessionDate = button.data('session-date'); // Extract info from data-* attributes
+    updateTimer(sessionDate); // Start the timer
+});
+
+var countdownInterval; // Global variable to keep track of the interval
+
+function updateTimer(sessionDate) {
+    // Clear any existing timer
+    clearInterval(countdownInterval);
+
+    var countDownDate = new Date(sessionDate).getTime();
+
+    countdownInterval = setInterval(function () {
+        var now = new Date().getTime();
+        var distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes, and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        $('#bookingModal1').on('show.bs.modal', function (e) {
+            // Reset the timer display to a default state or loading message
+            document.getElementById("timeLeft").innerHTML = "Loading...";
+        
+            var button = $(e.relatedTarget); // Button that triggered the modal
+            var sessionDate = button.data('session-date'); // Extract info from data-* attributes
+            updateTimer(sessionDate); // Start the timer
+        });
+        
+        // Update the timer display
+        document.getElementById("timeLeft").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+        // Disable booking if time is less than 24 hours
+        if (distance < 86400000) {
+            document.getElementById("bookingButton").disabled = true;
+        } else {
+            document.getElementById("bookingButton").disabled = false;
+        }
+
+        // If countdown is over
+        if (distance < 0) {
+            clearInterval(countdownInterval);
+            document.getElementById("timeLeft").innerHTML = "Booking closed";
+            document.getElementById("bookingButton").disabled = true;
+        }
+    }, 1000);
+}
+
